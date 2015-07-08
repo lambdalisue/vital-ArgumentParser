37c4b8f10a889e9f285eaa8df03d09204ded3fae
lambdalisue <lambdalisue@hashnote.net>
2015-07-08 18:02:03 +0900

    Update test env

--- test/run_themis.sh
+++ test/run_themis.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
-themis --runtimepath $HOME/.vim/bundle/vital.vim
+: ${VIMTHEMIS:=~/.vim/bundle/vim-themis}
+: ${VIMPROC:=~/.vim/bundle/vimproc.vim}
+
+# themis
+sh ${VIMTHEMIS}/bin/themis \
+     --reporter dot \
+     --runtimepath ${VIMPROC} \
+     $@
+
